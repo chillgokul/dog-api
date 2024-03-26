74e7a42da03dfe4a30a122d6a214fe718ff5bf40
chillgokul <gokchill@gmail.com>
2024-03-26 22:45:24 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-
 function App() {
+  const [dogImage, setDogImage] = useState('');
+
+  useEffect(() => {
+    fetchDogImage();
+  }, []);
+
+  const fetchDogImage = async () => {
+    try {
+      const response = await axios.get('https://api.thedogapi.com/v1/images/search');
+      setDogImage(response.data[0].url);
+    } catch (error) {
+      console.error('Error fetching dog image:', error);
+    }
+  };
+
+  const handleNewImageClick = () => {
+    fetchDogImage();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DOG IMAGES GENERATING API</h1>
+        <div className="dog-container">
+          <div className="dog-image-container">
+            {dogImage && <img src={dogImage} alt="Dog" />}
+          </div>
+        </div>
+        <button className="new-image-button" onClick={handleNewImageClick}>Get New Dog Image</button>
       </header>
     </div>
   );
+  
+  
+  
 }
 
 export default App;
